e282eba2d42ece3682e0396f6afff3f2070deab5
aulanbekuulu <aulanbekuulu@srs.lan>
2023-12-27 13:21:04 +0600

    migration added reward application

---
+++ src/Infrastructure/Migrations/20231227071941_addedRewardApplicationStatuses.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedRewardApplicationStatuses : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "RewardApplicationStatuses",
+                schema: "main",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    RewardApplicationId = table.Column<int>(type: "integer", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    ChangeDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RewardApplicationStatuses", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RewardApplicationStatuses_reward_applications_RewardApplica~",
+                        column: x => x.RewardApplicationId,
+                        principalSchema: "main",
+                        principalTable: "reward_applications",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RewardApplicationStatuses_RewardApplicationId",
+                schema: "main",
+                table: "RewardApplicationStatuses",
+                column: "RewardApplicationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RewardApplicationStatuses",
+                schema: "main");
+        }
+    }
+}
